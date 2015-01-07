cd69fed4f85713316bed1bb058f17b9899c681bf
Eric Demko <edemko@newfields.com>
2015-01-07 11:37:30 -0500

    now the encoder recognizes the __jsonfields__ attr to build json

--- flask_json/__init__.py
+++ flask_json/__init__.py
@@ -18,12 +18,32 @@ class _Encoder(json.JSONEncoder):
     bytype = []
 
     def default(self, obj):
-        for type, __json__ in _Encoder.bytype:
-            if isinstance(obj, type):
+        for the_type, __json__ in _Encoder.bytype:
+            if isinstance(obj, the_type):
                 return __json__(obj)
+        if hasattr(obj, '__jsonfields__'):
+            return self._jsonfields(obj, obj.__jsonfields__)
         if hasattr(obj, '__json__'):
             return obj.__json__()
         return super(_Encoder, self).default(obj)
+
+    def _jsonfields(self, obj, fields):
+        data = dict()
+        def autofields(lst):
+            for name in lst:
+                data[name] = getattr(obj, name)
+        if isinstance(fields, dict):
+            for key, val in fields.items():
+                if key == '__auto__':
+                    autofields(val)
+                elif callable(val):
+                    data[key] = val(obj)
+                else:
+                    data[key] = val
+        else:
+            autofields(fields)
+        return data
+
 
 def jsonEncode(ty):
     if not isinstance(ty, type):
