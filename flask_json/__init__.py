0fe8ee9c12a84072b91dfe9ca923adfa22fa6776
Eric Demko <edemko@newfields.com>
2015-01-07 10:50:08 -0500

    fixed encoder (super issue & added by-type encoders)

--- flask_json/__init__.py
+++ flask_json/__init__.py
@@ -13,6 +13,30 @@ class Json:
         app.json = MethodType(json_route, app)
         return self
 
+
+class _Encoder(json.JSONEncoder):
+    bytype = []
+
+    def default(self, obj):
+        for type, __json__ in _Encoder.bytype:
+            if isinstance(obj, type):
+                return __json__(obj)
+        if hasattr(obj, '__json__'):
+            return obj.__json__()
+        return super(_Encoder, self).default(obj)
+
+def jsonEncode(ty):
+    if not isinstance(ty, type):
+        raise TypeError("Must call the jsonEncode decorator with a type.")
+    def decorate(__json__):
+        _Encoder.bytype.insert(0, (ty, __json__))
+        return __json__
+    return decorate
+
+def dumps(obj):
+    return json.dumps(obj, cls=_Encoder)
+
+
 def json_route(app, route, **kwargs):
     def decorate(handler):
         @wraps(handler)
@@ -20,19 +44,8 @@ def json_route(app, route, **kwargs):
             # TODO check accept header
             obj = handler(*args, **kwargs)
             # TODO when an error occurs, provide JSON error pages
-            # TODO figure out how to extend json.dumps
-            it = json.dumps(obj, cls=_Encoder)
+            it = dumps(obj)
             return Response(it, mimetype='application/json')
         app.route(route, **kwargs)(inner)
         return inner
     return decorate
-
-class _Encoder(json.JSONEncoder):
-    def default(self, obj):
-        # FIXME check a registry of type-function pairs
-        for type, func in []:
-            if isinstance(obj, type):
-                return func(obj)
-        if hasattr(obj, '__json__'):
-            return obj.__json__()
-        return super(json.JSONEncoder, self).default(obj)
